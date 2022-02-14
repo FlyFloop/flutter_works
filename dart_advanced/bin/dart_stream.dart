82a2f44f20176aa10ef0b4a3b96f2579a223a588
Alper <alperyorgun2@gmail.com>
2022-02-15 02:54:31 +0300

    my bitch love coco

--- dart_advanced/bin/dart_stream.dart
+++ dart_advanced/bin/dart_stream.dart
@@ -18,15 +18,30 @@ void main(List<String> args) {
   void functionForStreamController() async {
     for (var i = 0; i < 10; i++) {
       await Future.delayed(Duration(seconds: 1));
+      //bir hata oluşturalım
+      if (i == 6) _streamController.addError('bir hata oluştu'); //hata ekledik
       _streamController.sink.add(i + 1);
     }
+    _streamController.addError('bir hata oluştu'); //hata ekledik
+    _streamController
+        .close(); //streami kapattık ve yayın yapan herkes kapandığını söylüyor
   }
 
   functionForStreamController();
   //ardından doldurduğumuz streami dinliyoruz
   _streamController.stream.listen((event) {
     print(event);
-  });
+  },
+      //stream dinlemesi bitince neler yapılacağı onDone'da tanımlanır
+      onDone: () {
+    print('streamcontroller listen done');
+  },
+      //listenin işlemi yapılırken hata çıktığında neler yapılacağı enError'da tanımlanır
+      onError: (error) {
+    print('streamcontroller listen error hata :');
+    print(error.toString());
+  }, cancelOnError: true //bir hata ile karşılaşıldığında işlemleri durduruyor
+      );
 }
 
 Stream<int> myStreamFunction() async* {
