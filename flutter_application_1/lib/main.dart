99e5b826c14df9ea1bbebce2de43db7cc08d8a74
Alper <alperyorgun2@gmail.com>
2022-03-13 04:08:14 +0300

    okay okay 2

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/controller/signup_controller.dart';
 import 'package:flutter_application_1/screens/auth/signup_screen.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value) {
+    Get.put(SignupController());
+  });
   runApp(MainPage());
 }
 
